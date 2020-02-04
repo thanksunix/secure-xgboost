14f87f1500b2a1596ed24edcb919696cf978671d
枢言 <shuyan.ycf@antfin.com>
2020-02-04 13:56:37 +0800

    Gain another 2x speedup by inline ObliviousAssign.

--- src/common/obl_primitives.h
+++ src/common/obl_primitives.h
@@ -47,14 +47,14 @@ inline void ObliviousSort(Iter begin, Iter end, Comparator cmp);
 template <typename T>
 inline T ObliviousArrayAccess(const T *arr, size_t i, size_t n);
 
-inline void ObliviousArrayAccessBytes(void *dst, const void *array, size_t nbytes,
-                                      size_t i, size_t n);
+inline void ObliviousArrayAccessBytes(void *dst, const void *array,
+                                      size_t nbytes, size_t i, size_t n);
 
 template <typename T>
 inline void ObliviousArrayAssign(T *arr, size_t i, size_t n, const T &val);
 
-inline void ObliviousArrayAssignBytes(void *array, const void *src, size_t nbytes,
-                                      size_t i, size_t n);
+inline void ObliviousArrayAssignBytes(void *array, const void *src,
+                                      size_t nbytes, size_t i, size_t n);
 
 // Impl.
 
@@ -192,24 +192,24 @@ inline void ObliviousArrayAssign(T *arr, size_t i, size_t n, const T &val) {
   return ObliviousArrayAssignBytes(arr, &val, sizeof(T), i, n);
 }
 
-inline void ObliviousArrayAssignBytes(void *array, const void *src, size_t nbytes,
-                                      size_t i, size_t n) {
+inline void ObliviousArrayAssignBytes(void *array, const void *src,
+                                      size_t nbytes, size_t i, size_t n) {
   size_t step = nbytes < CACHE_LINE_SIZE ? CACHE_LINE_SIZE / nbytes : 1;
   for (size_t j = 0; j < n; j += step) {
     bool cond = ObliviousEqual(j / step, i / step);
     int pos = ObliviousChoose(cond, i, j);
-    void* dst_pos = (char*)(array) + pos * nbytes;
+    void *dst_pos = (char *)(array) + pos * nbytes;
     obl::ObliviousBytesAssign(cond, nbytes, src, dst_pos, dst_pos);
   }
 }
 
-inline void ObliviousArrayAccessBytes(void* dst, const void *array, size_t nbytes,
-                                      size_t i, size_t n) {
+inline void ObliviousArrayAccessBytes(void *dst, const void *array,
+                                      size_t nbytes, size_t i, size_t n) {
   size_t step = nbytes < CACHE_LINE_SIZE ? CACHE_LINE_SIZE / nbytes : 1;
   for (size_t j = 0; j < n; j += step) {
     bool cond = ObliviousEqual(j / step, i / step);
     int pos = ObliviousChoose(cond, i, j);
-    void* src_pos = (char*)(array) + pos * nbytes;
+    void *src_pos = (char *)(array) + pos * nbytes;
     obl::ObliviousBytesAssign(cond, nbytes, src_pos, dst, dst);
   }
 }
@@ -319,3 +319,97 @@ inline void ObliviousSort(Iter begin, Iter end, Comparator cmp) {
   value_type *array = &(*begin);
   return detail::o_sort<value_type, Comparator>(array, 0, end - begin, cmp);
 }
+
+namespace obl {
+
+template <typename T,
+          typename std::enable_if<!std::is_same<T, uint8_t>::value &&
+                                      std::is_scalar<T>::value,
+                                  int>::type = 0>
+inline void ObliviousAssignHelper(bool pred, T t_val, T f_val, T *out) {
+#ifdef SIMULATED_OBL_ASSIGN_HELPER
+  *out = pred ? t_val : f_val;
+#else
+  T result;
+  __asm__ volatile(
+      "mov %2, %0;"
+      "test %1, %1;"
+      "cmovz %3, %0;"
+      : "=&r"(result)
+      : "r"(pred), "r"(t_val), "r"(f_val), "m"(out)
+      : "cc");
+  *out = result;
+#endif
+}
+
+template <typename T, typename std::enable_if<std::is_same<T, uint8_t>::value,
+                                              int>::type = 0>
+inline void ObliviousAssignHelper(bool pred, T t_val, T f_val, T *out) {
+#ifdef SIMULATED_OBL_ASSIGN_HELPER
+  *out = pred ? t_val : f_val;
+#else
+  uint16_t result;
+  uint16_t t = t_val;
+  uint16_t f = f_val;
+  __asm__ volatile(
+      "mov %2, %0;"
+      "test %1, %1;"
+      "cmovz %3, %0;"
+      : "=&r"(result)
+      : "r"(pred), "r"(t), "r"(f), "m"(out)
+      : "cc");
+  *out = static_cast<uint8_t>(result);
+#endif
+}
+
+inline void ObliviousBytesAssign(bool pred, size_t nbytes, const void *t_val,
+                                 const void *f_val, void *out) {
+#ifdef SIMULATED_OBL_ASSIGN
+  if (pred)
+    std::memcpy(out, t_val, nbytes);
+  else
+    std::memcpy(out, f_val, nbytes);
+#else
+
+  const size_t bytes = nbytes;
+  char *res = (char *)out;
+  char *t = (char *)t_val;
+  char *f = (char *)f_val;
+
+  // Obliviously assign 8 bytes at a time
+  size_t num_8_iter = bytes / 8;
+#pragma omp simd
+  for (int i = 0; i < num_8_iter; i++) {
+    ObliviousAssignHelper(pred, *((uint64_t *)t), *((uint64_t *)f),
+                          (uint64_t *)res);
+    res += 8;
+    t += 8;
+    f += 8;
+  }
+
+  // Obliviously assign 4 bytes
+  if ((bytes % 8) / 4) {
+    ObliviousAssignHelper(pred, *((uint32_t *)t), *((uint32_t *)f),
+                          (uint32_t *)res);
+    res += 4;
+    t += 4;
+    f += 4;
+  }
+
+  // Obliviously assign 2 bytes
+  if ((bytes % 4) / 2) {
+    ObliviousAssignHelper(pred, *((uint16_t *)t), *((uint16_t *)f),
+                          (uint16_t *)res);
+    res += 2;
+    t += 2;
+    f += 2;
+  }
+
+  if ((bytes % 2)) {
+    ObliviousAssignHelper(pred, *((uint8_t *)t), *((uint8_t *)f),
+                          (uint8_t *)res);
+  }
+#endif
+}
+
+}  // namespace obl
